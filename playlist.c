bc70e8abc262f0842ad8735c6d25919b601f4f7f
holly-groves <43141062+holly-groves@users.noreply.github.com>
2019-10-25 15:51:50 +0100

    inital files

---
+++ playlist.c
@@ -0,0 +1,273 @@
+/*
+	Name: Holly Groves
+	Matric: 180007261
+	Module: AC21008
+	Playlist Assignment
+		Doubly Linked List
+*/
+
+#include "playlist.h"
+#include <stdlib.h>
+#include <string.h>
+#include<stdio.h>
+
+/*
+	create a new playlist (a new doubly linked list)
+	parameters:
+		listPtr - receives a pointer to the new doubly linked list
+*/
+int createPlaylist(Playlist **listPtr){
+	//check that the memory address was provided
+	if (listPtr == NULL)
+		return INVALID_INPUT_PARAMETER;
+
+	//check that the list has not been created already
+	if (*listPtr != NULL)
+		return INVALID_INPUT_PARAMETER;
+
+	//allocate memory for the new list and store in the pointer
+	(*listPtr) = (Playlist*)myMalloc(sizeof(Playlist));
+
+	//check that memory allocation was successful
+	if ((*listPtr) == NULL)
+		return MEMORY_ALLOCATION_ERROR;
+
+	//initialise fields in the list (playlist)
+	(*listPtr)->head = NULL;
+	(*listPtr)->tail = NULL;
+	(*listPtr)->curr = NULL;
+
+	return SUCCESS;
+}
+
+/*
+	insert a new track before the current track (insert a new node before the current node in the list)
+	parameters:
+		listPtr - a pointer to the playlist
+		trackName - name of the track to add
+		trackLength - the length of the track to add
+*/
+int insertBeforeCurr(Playlist* listPtr, char trackName[], int trackLength) {
+	//input validation
+	if (trackName == NULL)
+		return INVALID_INPUT_PARAMETER;
+	int strLength; 
+	strLength = strlen(trackName); //get the length of the track name
+	if (strLength > 50) //validate name
+		return INVALID_INPUT_PARAMETER;
+		
+	if (trackLength == 0) //validate track length
+		return INVALID_INPUT_PARAMETER;
+
+	//check the pointer given is valid
+	if (listPtr == NULL)
+		return INVALID_INPUT_PARAMETER;
+
+	//create a new track to go into the playlist
+	MP3Track *newTrack = (MP3Track*)myMalloc(sizeof(MP3Track));
+	//check memory allocation was successful
+	if (newTrack == NULL)
+		return MEMORY_ALLOCATION_ERROR;
+
+	//initialise fields in new node
+	strcpy(newTrack->trackName, trackName);
+	newTrack->trackLength = trackLength;
+	newTrack->prev = NULL;
+	newTrack->next = NULL;
+
+	
+	if (listPtr->head == NULL && listPtr->tail == NULL) //if the playlist is empty
+	{
+		listPtr->head == newTrack;
+		listPtr->tail == newTrack;
+		listPtr->curr == newTrack;
+	}
+	else //list isn't empty
+	{
+		if (listPtr->curr->prev == NULL) //if the current track is at the head of the list
+		{
+			newTrack->next = listPtr->curr;
+			listPtr->curr->prev = newTrack;
+			listPtr->head = newTrack;
+		}
+		else //if the current track is not the head of the list
+		// if (listPtr->curr->prev != NULL) 
+		{
+			newTrack->next = listPtr->curr;
+			newTrack->prev = listPtr->curr->prev;
+			listPtr->curr->prev->next = newTrack;
+			listPtr->curr->prev = newTrack;
+			//move curr one position back in the list
+			listPtr->curr = listPtr->curr->prev;
+		}
+	}
+
+	return SUCCESS;
+}
+
+/*
+	insert a new track after the current track (insert a new node after the current node)
+	parameters:
+		listPtr - a pointer to the playlist
+		trackName - name of the track to be added
+		trackLength - length of the track to be added
+*/
+int insertAfterCurr(Playlist* listPtr, char trackName[], int trackLength){
+	//input validation
+	if (trackName == NULL)
+		return INVALID_INPUT_PARAMETER;
+	int strLength; 
+	strLength = strlen(trackName); //get the length of the track name
+	if (strLength > 50) //validate name
+		return INVALID_INPUT_PARAMETER;
+	if (trackLength == 0) //validate track length
+		return INVALID_INPUT_PARAMETER;
+
+	//check the pointer given is valid
+	if (listPtr == NULL)
+		return INVALID_INPUT_PARAMETER;
+
+	//create a new track to go into the playlist
+	MP3Track *newTrack = (MP3Track*)myMalloc(sizeof(MP3Track));
+	//check memory allocation was successful
+	if (newTrack == NULL)
+		return MEMORY_ALLOCATION_ERROR;
+
+	//initialise fields in new node
+	strcpy(newTrack->trackName, trackName);
+	newTrack->trackLength = trackLength;
+	newTrack->prev = NULL;
+	newTrack->next = NULL;
+
+	if (listPtr->head == NULL && listPtr->tail == NULL && listPtr->curr == NULL) //if the playlist is empty
+	{
+		listPtr->head == newTrack;
+		listPtr->tail == newTrack;
+		listPtr->curr == newTrack;
+	}
+	else //list isn't empty
+	{
+		if (listPtr->curr == listPtr->tail && listPtr->curr->next == NULL) //if the current track is at the tail of the list
+		{
+			newTrack->prev = listPtr->curr;
+			listPtr->curr->next = newTrack;	
+			listPtr->tail = newTrack;
+		}
+		else //if the current track is not at the tail of the list
+		// if (listPtr->curr->next != NULL) 
+		{
+			newTrack->prev = listPtr->curr;
+			newTrack->next = listPtr->curr->next;
+			listPtr->curr->next->prev = newTrack;
+			listPtr->curr->next = newTrack;
+		}
+	}
+
+	return SUCCESS;
+}
+
+/*
+	go to the next track in the playlist (make the next node after the current node the new current node)
+	paramters:
+		listPtr - a pointer to the playlist
+*/
+int skipNext(Playlist* listPtr){
+	//validation
+	if (listPtr == NULL) //if list is not initialised
+		return INVALID_INPUT_PARAMETER;
+	if (listPtr->curr == NULL || listPtr->curr->next == NULL) //if list is empty or current track is at the tail
+		return INVALID_LIST_OPERATION;
+	
+	listPtr->curr = listPtr->curr->next;
+
+	return SUCCESS;
+}
+
+/*
+	go to the previous track in the playlist (make the node before the current node the new current node)
+	parameters:
+		listPtr - a pointer to the playlist
+*/
+int skipPrev(Playlist* listPtr){
+	//validation
+	if (listPtr == NULL) //if list is not initialised
+		return INVALID_INPUT_PARAMETER;
+	if (listPtr->curr == NULL  || listPtr->curr->next == NULL) //if list is empty or current track is at the head
+		return INVALID_LIST_OPERATION;
+	
+	listPtr->curr = listPtr->curr->prev;
+
+	return SUCCESS;
+}
+
+/*
+	get the information for the current track in the playlist
+	parameters:
+		listPtr - a pointer to the playlist
+		pTrack - a pointer which will recieve the current track
+*/
+int getCurrentTrack(Playlist* listPtr, MP3Track *pTrack){
+	//validation
+	if (listPtr == NULL) //check that it is a valid list
+		return INVALID_INPUT_PARAMETER;
+
+	if (pTrack == NULL) //check it is a valid pointer
+		return INVALID_INPUT_PARAMETER;
+
+	// if (listPtr->curr == NULL) //check that there is a track in the list to get info from
+	// 	return INVALID_LIST_OPERATION;
+
+	//store the track at current
+	pTrack = listPtr->curr;
+
+	return SUCCESS;
+}
+
+/*
+	remove the current track from the playlist
+	LOOK AT HEADER FILE FOR MORE INFO
+	parameters:
+		listPtr -
+		pTrack -
+		moveForward -
+*/
+int removeAtCurr(Playlist* listPtr, MP3Track *pTrack, int moveForward){
+	(void)listPtr;
+	(void)pTrack;
+	(void)moveForward;
+	return NOT_IMPLEMENTED;
+}
+
+/*
+	empty the contents of the playlist (empty the contents of the doubly linked list)
+	parameters:
+		listPtr -
+*/
+int clearPlaylist(Playlist* listPtr){
+	(void)listPtr;
+	return NOT_IMPLEMENTED;
+}
+
+/*
+	save the contents of a playlist to a file (save the information of the doubly linked list)
+	parameters:
+		listPtr -
+		filename -
+*/
+int savePlaylist(Playlist *listPtr, char filename[]){
+	(void)listPtr;
+	(void)filename;
+	return NOT_IMPLEMENTED;
+}
+
+/*
+	creates a new playlist (a new list) and loads the contents from a file
+	paramters:
+		listPtr -
+		filename -
+*/
+int loadPlaylist(Playlist **listPtr, char filename[]){
+	(void)listPtr;
+	(void)filename;
+	return NOT_IMPLEMENTED;
+}
